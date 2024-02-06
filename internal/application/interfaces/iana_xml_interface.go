6aaed2d9acdf49e1560839524b810573ec896f5e
Geoff <geoffrey@deprins.net>
2024-02-06 16:30:52 -0600

    remove xml service

--- internal/application/interfaces/iana_xml_interface.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/onasunnymorning/domain-os/internal/domain/entities"
-
-type IANAXMLService interface {
-	ListIANARegistrars() ([]*entities.IANARegistrar, error)
-}
