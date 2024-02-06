7bdeb526b3ba782271c965eca3fc4f4dce659b30
Geoff <geoffrey@deprins.net>
2024-02-06 17:38:37 -0600

    rename

--- internal/infrastructure/db/postgres/registrar_repository.go
+++ internal/infrastructure/db/postgres/registrar_repository.go
@@ -2,10 +2,47 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/onasunnymorning/domain-os/internal/domain/entities"
 	"gorm.io/gorm"
 )
+
+// Registrar is the GORM representation of a Registrar
+type Registrar struct {
+	ClID        string `gorm:"primary_key"`
+	Name        string `gorm:"unique;not null"`
+	NickName    string `gorm:"unique;not null"`
+	GurID       int
+	Email       string
+	Status      string `gorm:"not null"`
+	Street1Int  string
+	Street2Int  string
+	Street3Int  string
+	CityInt     string
+	SPInt       string `gorm:"column:sp_int"`
+	PCInt       string `gorm:"column:pc_int"`
+	CCInt       string `gorm:"column:cc_int"`
+	Street1Loc  string
+	Street2Loc  string
+	Street3Loc  string
+	CityLoc     string
+	SPLoc       string `gorm:"column:sp_loc"`
+	PCLoc       string `gorm:"column:pc_loc"`
+	CCLoc       string `gorm:"column:cc_loc"`
+	Voice       string
+	Fax         string
+	URL         string
+	Whois43     string
+	Whois80     string
+	RdapBaseUrl string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+func (Registrar) TableName() string {
+	return "registrars"
+}
 
 // GormRegistrarRepository implements the RegistrarRepository interface
 type GormRegistrarRepository struct {
