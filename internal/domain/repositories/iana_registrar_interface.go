c672659f1635db07c5bcaa178cba4be312c1b01d
Geoff <geoffrey@deprins.net>
2024-02-06 16:27:34 -0600

    update ianaRegsitrar_interface

--- internal/domain/repositories/iana_registrar_interface.go
+++
@@ -1,7 +0,0 @@
-package repositories
-
-import "github.com/onasunnymorning/domain-os/internal/domain/entities"
-
-type IANARegistrarRepository interface {
-	ListIANARegistrars() ([]*entities.IANARegistrar, error)
-}
