7c9748b5f5095c0632383c0cc407fc8eee25bdd2
Geoff <geoffrey@deprins.net>
2024-02-10 13:20:08 -0600

    registrar repo tests

---
+++ internal/infrastructure/db/postgres/registrar_repository_test.go
@@ -0,0 +1,129 @@
+package postgres
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onasunnymorning/domain-os/internal/domain/entities"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
+)
+
+type RegistrarSuite struct {
+	suite.Suite
+	db *gorm.DB
+}
+
+func TestRegistrarSuite(t *testing.T) {
+	suite.Run(t, new(RegistrarSuite))
+}
+
+func (s *RegistrarSuite) SetupSuite() {
+	s.db = getTestDB()
+	NewGormTLDRepo(s.db)
+}
+
+func (s *RegistrarSuite) TestCreateRegistrar() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar, err := repo.Create(context.Background(), registrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar)
+}
+
+func (s *RegistrarSuite) TestCreateRegistrar_Duplicate() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar, err := repo.Create(context.Background(), registrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar)
+
+	// Create a duplicate
+	createdRegistrar, err = repo.Create(context.Background(), registrar)
+	require.Error(s.T(), err)
+	require.Nil(s.T(), createdRegistrar)
+}
+
+func (s *RegistrarSuite) TestReadRegistrar() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar, err := repo.Create(context.Background(), registrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar)
+
+	readRegistrar, err := repo.GetByClID(context.Background(), registrar.ClID.String())
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), readRegistrar)
+	require.Equal(s.T(), createdRegistrar, readRegistrar)
+}
+
+func (s *RegistrarSuite) TestUpdateRegistrar() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar, err := repo.Create(context.Background(), registrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar)
+
+	createdRegistrar.Name = "Updated Registrar Name"
+	updatedRegistrar, err := repo.Update(context.Background(), createdRegistrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), updatedRegistrar)
+	require.Equal(s.T(), "Updated Registrar Name", updatedRegistrar.Name)
+}
+
+func (s *RegistrarSuite) TestDeleteRegistrar() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar, err := repo.Create(context.Background(), registrar)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar)
+
+	err = repo.Delete(context.Background(), createdRegistrar.ClID.String())
+	require.NoError(s.T(), err)
+
+	_, err = repo.GetByClID(context.Background(), createdRegistrar.ClID.String())
+	require.Error(s.T(), err)
+
+	err = repo.Delete(context.Background(), createdRegistrar.ClID.String())
+	require.NoError(s.T(), err)
+
+	_, err = repo.GetByClID(context.Background(), createdRegistrar.ClID.String())
+	require.Error(s.T(), err)
+}
+
+func (s *RegistrarSuite) TestListRegistrars() {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+	repo := NewGormRegistrarRepository(tx)
+
+	registrar1, _ := entities.NewRegistrar("my-registrar-id", "Gomamma Inc.", "contact@gomamma.com", 12345)
+	createdRegistrar1, err := repo.Create(context.Background(), registrar1)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar1)
+
+	registrar2, _ := entities.NewRegistrar("my-registrar-id2", "GoBro Inc.", "contact@gobro.com", 12346)
+	createdRegistrar2, err := repo.Create(context.Background(), registrar2)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), createdRegistrar2)
+
+	registrars, err := repo.List(context.Background(), 25, "")
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), registrars)
+	require.Len(s.T(), registrars, 2)
+}
