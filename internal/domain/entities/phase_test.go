3d289832f694c1194d08966cd2b410647f1769eb
Geoff <geoffrey@deprins.net>
2024-03-30 11:17:00 -0500

    AddFee testing

--- internal/domain/entities/phase_test.go
+++ internal/domain/entities/phase_test.go
@@ -55,3 +55,114 @@ func TestNewPhase(t *testing.T) {
 		})
 	}
 }
+
+func TestPhase_AddFee(t *testing.T) {
+	tr := true
+	tc := []struct {
+		name        string
+		fees        []Fee
+		expectedErr error
+	}{
+		{
+			name: "NoErr",
+			fees: []Fee{
+				{
+					Name:       "fee1",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "NoErr One Fee Multiple Currencies",
+			fees: []Fee{
+				{
+					Name:       "fee1",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee1",
+					Currency:   "EUR",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee1",
+					Currency:   "GBP",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee1",
+					Currency:   "CHF",
+					Refundable: &tr,
+					Amount:     100,
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "NoErr Multiple Fees and Currencies",
+			fees: []Fee{
+				{
+					Name:       "fee1",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee1",
+					Currency:   "EUR",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee2",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee3",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "Duplicate Fees",
+			fees: []Fee{
+				{
+					Name:       "fee1",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     100,
+				},
+				{
+					Name:       "fee1",
+					Currency:   "USD",
+					Refundable: &tr,
+					Amount:     1000,
+				},
+			},
+			expectedErr: ErrDuplicateFeeEntry,
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			phase := &Phase{}
+			var err error
+			for _, fee := range tt.fees {
+				_, err = phase.AddFee(fee)
+			}
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
