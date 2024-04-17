a5eb9a96c282795fb4bbe8692939933b4c6fd560
Geoff <geoffrey@deprins.net>
2024-04-17 12:39:41 -0500

    rdeContact.ToEntity() + tests

--- internal/domain/entities/rdeContact.go
+++ internal/domain/entities/rdeContact.go
@@ -2,8 +2,10 @@ package entities
 
 import (
 	"encoding/xml"
+	"time"
 )
 
+// RDEContact  is a struct that facilitates the parsing of the Contact elements in the RDE XML
 type RDEContact struct {
 	XMLName    xml.Name               `xml:"contact"`
 	ID         string                 `xml:"id"`
@@ -19,6 +21,95 @@ type RDEContact struct {
 	UpRr       string                 `xml:"upRr"`
 	UpDate     string                 `xml:"upDate"`
 	Disclose   RDEDisclose            `xml:"disclose"`
+}
+
+// ToEntity converts the RDEContact to an Contact entity
+func (c *RDEContact) ToEntity() (*Contact, error) {
+	postalInfos := [2]*ContactPostalInfo{}
+	for i, postal := range c.PostalInfo {
+		postalInfo, err := postal.ToEntity()
+		if err != nil {
+			return nil, err
+		}
+		postalInfos[i] = postalInfo
+	}
+	disclose, err := c.Disclose.ToEntity()
+	if err != nil {
+		return nil, err // Untestable until implementation of Disclose.ToEntity()
+	}
+	// Since the Escrow specification (RFC 9022) does not specify the authInfo field, we will generate a random one to import the data
+	aInfo, err := NewAuthInfoType("escr0W1mP*rt")
+	if err != nil {
+		return nil, err // Untestable, just catching the error incase our AuthInfoType is validation changes
+	}
+	// Create a new contact object
+	contact, err := NewContact(c.ID, c.RoID, c.Email, aInfo.String(), c.ClID)
+	if err != nil {
+		return nil, err
+	}
+	// Add the postal info and disclose to the contact
+	contact.PostalInfo = postalInfos
+	contact.Disclose = *disclose
+
+	// Set the optional fields
+	if c.Voice != "" {
+		v, err := NewE164Type(c.Voice)
+		if err != nil {
+			return nil, err
+		}
+		contact.Voice = *v
+	}
+	if c.Fax != "" {
+		f, err := NewE164Type(c.Fax)
+		if err != nil {
+			return nil, err
+		}
+		contact.Fax = *f
+	}
+	if c.CrDate != "" {
+		date, err := time.Parse(time.RFC3339, c.CrDate)
+		if err != nil {
+			return nil, err
+		}
+		contact.CreatedAt = date
+	}
+	if c.UpDate != "" {
+		date, err := time.Parse(time.RFC3339, c.UpDate)
+		if err != nil {
+			return nil, err
+		}
+		contact.UpdatedAt = date
+	}
+
+	if c.CrRr != "" {
+		crrr, err := NewClIDType(c.CrRr)
+		if err != nil {
+			return nil, err
+		}
+		contact.CrRr = crrr
+	}
+	if c.UpRr != "" {
+		uprr, err := NewClIDType(c.UpRr)
+		if err != nil {
+			return nil, err
+		}
+		contact.UpRr = uprr
+	}
+
+	// Set the statuses
+	for _, status := range c.Status {
+		err := contact.SetStatus(ContactStatusType(status.S))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Validate the contact and return it
+	if _, err := contact.IsValid(); err != nil {
+		return nil, err
+	}
+	return contact, nil
+
 }
 
 // RDEContactPostalInfo is a struct that facilitates the parsing of the postalInfo element in the RDE XML
@@ -54,6 +145,7 @@ type RDEContactStatus struct {
 	S string `xml:"s,attr"`
 }
 
+// RDEDisclose is a struct that facilitates the parsing of the disclose element in the RDE XML
 type RDEDisclose struct {
 	Flag  bool                 `xml:"flag,attr"`
 	Name  []RDEContactWithType `xml:"name"`
@@ -62,6 +154,18 @@ type RDEDisclose struct {
 	Voice []string             `xml:"voice"`
 	Fax   []string             `xml:"fax"`
 	Email []string             `xml:"email"`
+}
+
+// ToEntity converts the RDEDisclose to an Disclose entity
+func (d *RDEDisclose) ToEntity() (*ContactDisclose, error) {
+	// our main data policy is not to disclose
+	cd := NewDiscloseStruct(false)
+	// Since our default is not to disclose, and some elements need to be set to false, we might skip this as everything is already set to false
+	if d.Flag {
+		// If the xml element is present, set the Disclose Property in question to equal the d.Flag (in this case true since we already handled the false case)
+		// TODO: implement this
+	}
+	return cd, nil
 }
 
 type RDEContactWithType struct {
--- internal/domain/entities/rdeContact_test.go
+++ internal/domain/entities/rdeContact_test.go
@@ -2,6 +2,7 @@ package entities
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -111,6 +112,493 @@ func TestRDEContactPostalInfo_ToEntity(t *testing.T) {
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, tc.expected, actual)
+			}
+		})
+	}
+}
+
+func TestRDEContact_ToEntity(t *testing.T) {
+	// Test cases
+	tests := []struct {
+		name     string
+		contact  *RDEContact
+		expected *Contact
+		err      error
+	}{
+		{
+			name: "Valid contact with postal info",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: &Contact{
+				ID:        "123456",
+				Email:     "myemail@me.com",
+				RoID:      RoidType("123456_CONT-APEX"),
+				ClID:      "123456",
+				Voice:     E164Type("+51.123456"),
+				Fax:       E164Type("+51.123456"),
+				CrRr:      ClIDType("123456"),
+				UpRr:      ClIDType("123456"),
+				CreatedAt: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				Status: ContactStatus{
+					OK: true,
+				},
+				PostalInfo: [2]*ContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: &Address{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: &Address{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Invalid postal info",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "invalid",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidPostalInfoEnumType,
+		},
+		{
+			name: "Invalid RoID",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_DOM-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidContactRoID,
+		},
+		{
+			name: "Invalid Voice",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidE164Type,
+		},
+		{
+			name: "Invalid Fax",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidE164Type,
+		},
+		// {
+		// 	name: "Invalid CrDate",
+		// 	contact: &RDEContact{
+		// 		ID:     "123456",
+		// 		RoID:   "123456_CONT-APEX",
+		// 		Email:  "myemail@me.com",
+		// 		ClID:   "123456",
+		// 		Voice:  "+51.123456",
+		// 		Fax:    "+51.123456",
+		// 		CrRr:   "123456",
+		// 		UpRr:   "123456",
+		// 		CrDate: "2021--01-01T00:00:00Z",
+		// 		UpDate: "2021-01-01T00:00:00Z",
+		// 		Status: []RDEContactStatus{
+		// 			{
+		// 				S: "ok",
+		// 			},
+		// 		},
+		// 		PostalInfo: []RDEContactPostalInfo{
+		// 			{
+		// 				Type: "int",
+		// 				Name: "myName",
+		// 				Org:  "myOrganization",
+		// 				Address: RDEAddress{
+		// 					City:        "New York",
+		// 					CountryCode: "US",
+		// 				},
+		// 			},
+		// 			{
+		// 				Type: "loc",
+		// 				Name: "myNûme",
+		// 				Org:  "myOrganïzation",
+		// 				Address: RDEAddress{
+		// 					City:        "Cûzco",
+		// 					CountryCode: "PE",
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// 	expected: nil,
+		// 	err:      ErrInvalidE164Type,
+		// },
+		// {
+		// 	name: "Invalid UpDate",
+		// 	contact: &RDEContact{
+		// 		ID:     "123456",
+		// 		RoID:   "123456_CONT-APEX",
+		// 		Email:  "myemail@me.com",
+		// 		ClID:   "123456",
+		// 		Voice:  "+51.123456",
+		// 		Fax:    "+51.123456",
+		// 		CrRr:   "123456",
+		// 		UpRr:   "123456",
+		// 		CrDate: "2021-01-01T00:00:00Z",
+		// 		UpDate: "2021--01-01T00:00:00Z",
+		// 		Status: []RDEContactStatus{
+		// 			{
+		// 				S: "ok",
+		// 			},
+		// 		},
+		// 		PostalInfo: []RDEContactPostalInfo{
+		// 			{
+		// 				Type: "int",
+		// 				Name: "myName",
+		// 				Org:  "myOrganization",
+		// 				Address: RDEAddress{
+		// 					City:        "New York",
+		// 					CountryCode: "US",
+		// 				},
+		// 			},
+		// 			{
+		// 				Type: "loc",
+		// 				Name: "myNûme",
+		// 				Org:  "myOrganïzation",
+		// 				Address: RDEAddress{
+		// 					City:        "Cûzco",
+		// 					CountryCode: "PE",
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// 	expected: nil,
+		// 	err:      ErrInvalidE164Type,
+		// },
+		{
+			name: "Invalid CrRr",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456789123456789",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidClIDType,
+		},
+		{
+			name: "Invalid UpRr",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456789123456789",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "ok",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidClIDType,
+		},
+		{
+			name: "Invalid Status",
+			contact: &RDEContact{
+				ID:     "123456",
+				RoID:   "123456_CONT-APEX",
+				Email:  "myemail@me.com",
+				ClID:   "123456",
+				Voice:  "+51.123456",
+				Fax:    "+51.123456",
+				CrRr:   "123456",
+				UpRr:   "123456",
+				CrDate: "2021-01-01T00:00:00Z",
+				UpDate: "2021-01-01T00:00:00Z",
+				Status: []RDEContactStatus{
+					{
+						S: "invalid",
+					},
+				},
+				PostalInfo: []RDEContactPostalInfo{
+					{
+						Type: "int",
+						Name: "myName",
+						Org:  "myOrganization",
+						Address: RDEAddress{
+							City:        "New York",
+							CountryCode: "US",
+						},
+					},
+					{
+						Type: "loc",
+						Name: "myNûme",
+						Org:  "myOrganïzation",
+						Address: RDEAddress{
+							City:        "Cûzco",
+							CountryCode: "PE",
+						},
+					},
+				},
+			},
+			expected: nil,
+			err:      ErrInvalidContactStatusCombination,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := tc.contact.ToEntity()
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.contact.ID, actual.ID.String())
+				require.Equal(t, tc.contact.RoID, actual.RoID.String())
+				require.Equal(t, tc.contact.ClID, actual.ClID.String())
+				require.Equal(t, tc.contact.CrRr, actual.CrRr.String())
+				require.Equal(t, tc.contact.UpRr, actual.UpRr.String())
+				require.Equal(t, tc.contact.Email, actual.Email)
+				require.Equal(t, len(tc.contact.PostalInfo), len(actual.PostalInfo))
+				require.Equal(t, tc.contact.Voice, actual.Voice.String())
+				require.Equal(t, tc.contact.Fax, actual.Fax.String())
+				// Test status
+				require.Equal(t, tc.expected.CreatedAt, actual.CreatedAt)
+				require.Equal(t, tc.expected.UpdatedAt, actual.UpdatedAt)
 			}
 		})
 	}
