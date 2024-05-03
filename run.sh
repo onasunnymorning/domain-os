7f27dda2934213bb63123b31ca114176fa34d812
Geoff <geoffrey@deprins.net>
2024-05-03 12:54:07 -0500

    env => doppler

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export BRANCH=$(git branch --show-current)
-docker build -t geapex/domain-os:$BRANCH . && docker compose -f docker-compose.yml up --abort-on-container-exit
-docker compose rm --force --volumes
+docker build -t geapex/domain-os:$BRANCH . && doppler run -- docker compose -f docker-compose.yml up --abort-on-container-exit
+doppler run -- docker compose rm --force --volumes
