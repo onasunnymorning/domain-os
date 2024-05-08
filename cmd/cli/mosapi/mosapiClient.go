64a4d220a41b26265edb49d9035ca9b8fab0a95f
Geoff <geoffrey@deprins.net>
2024-05-08 18:17:33 -0500

    mosapi client update

--- cmd/cli/mosapi/mosapiClient.go
+++ cmd/cli/mosapi/mosapiClient.go
@@ -24,21 +24,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(status.TLD, status.Status)
+	if status.Status == "Down" {
+		for s, service := range status.TestedServices {
+			log.Printf("Service %s for TLD %s is %s - emergency threshold percent : %v\n", s, status.TLD, service.Status, service.EmergencyThreshold)
+		}
+	} else {
+		log.Printf("TLD %s is %s\n", status.TLD, status.Status)
+	}
+
+	// jsondata, err := json.Marshal(status)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(string(jsondata))
 
 	// Get the DNS status of the TLD
-	alarmResponse, err := mosapiClient.GetAlarm("DNS")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(alarmResponse)
+	// alarmResponse, err := mosapiClient.GetAlarm("DNS")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(alarmResponse.IsAlarmed())
 
-	// Get the RDDS Downtime status of the TLD
-	downtimeResponse, err := mosapiClient.GetDowntime("RDDS")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(downtimeResponse)
+	// // Get the RDDS Downtime status of the TLD
+	// downtimeResponse, err := mosapiClient.GetDowntime("RDDS")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(downtimeResponse.Downtime)
 
 	// Logout
 	// err = mosapiClient.Logout()
--- internal/infrastructure/api/mosapi/stateResponse.go
+++ internal/infrastructure/api/mosapi/stateResponse.go
@@ -1,62 +1,31 @@
 package mosapi
 
-// Working CURL version
-// curl --cert icann-tls.crt.pem --key icann-tls.private.key https://mosapi-ote.icann.org/ry/example56/v2/monitoring/state
-
-// This struct is used to represent the response from the state endpoint of the MOSAPI
-// Ref:
-//
-//	{
-//		"tld": "example",
-//		"lastUpdateApiDatabase": 1496923082,
-//		"status": "Down",
-//		"testedServices": {
-//		"DNS": {
-//		"status": "Down",
-//		"emergencyThreshold": 10.0000,
-//		"incidents": [{
-//		"incidentID": "1495811850.1700",
-//		"endTime": null,
-//		"startTime": 1495811850,
-//		"falsePositive": false,
-//		"state": "Active"
-//		}]
-//		},
-//		"DNSSEC": {
-//		"status": "Down",
-//		"emergencyThreshold": 10.0000,
-//		"incidents": [{
-//		"incidentID": "1495811790.1694",
-//		"endTime": null,
-//		"startTime": 1495811790,
-//		"falsePositive": false,
-//		"state": "Active"
-//		}]
-//		},
-//		"EPP": {
-//		"status": "Disabled"
-//		},
-//		"RDDS": {
-//		13
-//		"status": "Disabled"
-//		},
-//		"RDAP": {
-//		"status": "Disabled"
-//		}
-//		},
-//		"version": 2
-//		}
 type StateResponse struct {
 	TLD             string `json:"tld"`
-	LastUpdateApiDb int    `json:"lastUpdateApiDatabase"`
-	Status          string `json:"status"`
-	TestedServices  map[string]TestedService
-	Version         int `json:"version"`
+	LastUpdateApiDb int    `json:"lastUpdateApiDatabase"` // a JSON number that contains the Unix time stamp of the date and time that the monitoring information provided in the MoSAPI was last updated from the monitoring system central database.
+	// Status: the current status of the Service. The possible values are:
+	// Up: all of the monitored Services are up.
+	// Down: one or more of the monitored Services are down.
+	// Up-inconclusive: the SLA monitoring system is under maintenance, therefore all the monitored Services of the TLD are considered to be up by default. Note: if the status is
+	// Up-inconclusive
+	Status         string `json:"status"`
+	TestedServices map[string]TestedService
+	Version        int `json:"version"`
 }
 
 // TestedService is a struct that represents a tested service in the MOSAPI
 type TestedService struct {
-	Status             string     `json:"status"`
+	// 	"status", a JSON string that contains the status of the Service as seen from the monitoring system. The "status" field can contain one of the following values:
+	// · Up: the monitored Service is up.
+	// · Down: the monitored Service is down.
+	// · Disabled: the Service is not being monitored.
+	// · UP-inconclusive-no-data: indicates that there are enough probe nodes online, but not enough raw data points were received to make a determination.
+	// · UP-inconclusive-no-probes: indicates that there are not enough probe nodes online to make a determination.
+	// · UP-inconclusive-reconfig: indicates that the system is undergoing a reconfiguration for the particular TLD and service.
+	Status string `json:"status"`
+	// "emergencyThreshold", a JSON number that contains the current percentage of the Emergency Threshold of the Service. Note: the value "0" specifies that the are no Incidents affecting the Emergency Threshold of the Service.
+	// Emergency Threshold: downtime threshold that if reached by any of the monitored Services may cause the TLD's Services emergency transition to an interim Registry Operator. To reach an Emergency Threshold a Service must accumulate X hours of total downtime during the last 7 days (i.e., rolling week).
+	// For DNS X=4 (4h per rolling week), for RDDS and RDAP X=24 (24h per rolling week)
 	EmergencyThreshold float64    `json:"emergencyThreshold"`
 	Incidents          []Incident `json:"incidents"`
 }
