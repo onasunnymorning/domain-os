e761f4c4b1bb7d2ed6086b6394f4ece34fa00bb8
Geoff <geoffrey@deprins.net>
2024-05-24 15:09:17 -0500

    +80% testing

--- internal/domain/entities/priceEngine_test.go
+++ internal/domain/entities/priceEngine_test.go
@@ -341,3 +341,452 @@ func TestAddPremiumFees(t *testing.T) {
 		})
 	}
 }
+
+func TestAddPhasePrice(t *testing.T) {
+	testcases := []struct {
+		name          string
+		domain        Domain
+		phase         Phase
+		fx            FX
+		pl            []*PremiumLabel
+		quoteRequest  QuoteRequest
+		expectedPrice int64
+	}{
+		{
+			name: "No Phase Price",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRenewal,
+				Currency:        "EUR",
+			},
+			expectedPrice: 0,
+		},
+		{
+			name: "Phase Price in target currency",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+				Prices: []Price{
+					{
+						RegistrationAmount: 100,
+						RenewalAmount:      100,
+						Currency:           "EUR",
+					},
+				},
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRenewal,
+				Currency:        "EUR",
+			},
+			expectedPrice: 100,
+		},
+		{
+			name: "Phase Price in base currency",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+				Policy: PhasePolicy{
+					BaseCurrency: "USD",
+				},
+				Prices: []Price{
+					{
+						RegistrationAmount: 100,
+						RenewalAmount:      100,
+						Currency:           "USD",
+					},
+				},
+			},
+			fx: FX{
+				BaseCurrency:   "USD",
+				TargetCurrency: "EUR",
+				Rate:           0.8,
+			},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRenewal,
+				Currency:        "EUR",
+			},
+			expectedPrice: 80,
+		},
+		{
+			name: "Phase Price in an odd currency",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+				Policy: PhasePolicy{
+					BaseCurrency: "USD",
+				},
+				Prices: []Price{
+					{
+						RegistrationAmount: 100,
+						RenewalAmount:      100,
+						Currency:           "PEN",
+					},
+				},
+			},
+			fx: FX{
+				BaseCurrency:   "USD",
+				TargetCurrency: "EUR",
+				Rate:           0.8,
+			},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRenewal,
+				Currency:        "EUR",
+			},
+			expectedPrice: 0,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			priceEngine := NewPriceEngine(tc.phase, tc.domain, tc.fx, tc.pl)
+			priceEngine.QuoteRequest = tc.quoteRequest
+			var err error
+			priceEngine.Quote, err = NewQuoteFromQuoteRequest(tc.quoteRequest)
+			priceEngine.setQuoteParams()
+			require.NoError(t, err, "Error creating Quote")
+			priceEngine.addPhasePrice()
+			require.Equal(t, tc.expectedPrice, priceEngine.Quote.Price.Amount(), "Price is not correct")
+		})
+	}
+}
+
+func TestGetQuote(t *testing.T) {
+	tr := true
+	testcases := []struct {
+		name          string
+		domain        Domain
+		phase         Phase
+		fx            FX
+		pl            []*PremiumLabel
+		quoteRequest  QuoteRequest
+		expectedQuote Quote
+		expectedError error
+	}{
+		{
+			name: "No Price",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRegistration,
+				Currency:        "EUR",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(0, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees:       nil,
+				FXRate:     &FX{},
+				Class:      "standard",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "invalid phase name",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRegistration,
+				Currency:        "EUR",
+				PhaseName:       "bullocks",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(0, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees:       nil,
+				FXRate:     &FX{},
+				Class:      "standard",
+			},
+			expectedError: ErrInvalidPhaseName,
+		},
+		{
+			name: "invalid domain name - quote request",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "exam--ple.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRegistration,
+				Currency:        "EUR",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(0, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees:       nil,
+				FXRate:     &FX{},
+				Class:      "standard",
+			},
+			expectedError: ErrInvalidQuoteRequest,
+		},
+		{
+			name: "just fees",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+				Fees: []Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+				},
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRegistration,
+				Currency:        "EUR",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(1100, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees: []*Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+				},
+				FXRate: &FX{},
+				Class:  "standard",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "fees with grandfathering",
+			domain: Domain{
+				Name: "example.com",
+				GrandFathering: DomainGrandFathering{
+					GFAmount:          100,
+					GFCurrency:        "EUR",
+					GFExpiryCondition: "transfer",
+				},
+			},
+			phase: Phase{
+				Name: "GA",
+				Fees: []Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+				},
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRenewal,
+				Currency:        "EUR",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(1200, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees: []*Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+					{
+						Name:       "grandfathered renewal fee",
+						Amount:     100,
+						Currency:   "EUR",
+						Refundable: &tr,
+					},
+				},
+				FXRate: &FX{},
+				Class:  "standard",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "fees with premiums",
+			domain: Domain{
+				Name: "example.com",
+			},
+			phase: Phase{
+				Name: "GA",
+				Fees: []Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+				},
+			},
+			fx: FX{},
+			pl: []*PremiumLabel{
+				{
+					Label:              "example",
+					RegistrationAmount: 100,
+					RenewalAmount:      100,
+					RestoreAmount:      100,
+					TransferAmount:     100,
+					Currency:           "EUR",
+					Class:              "lowPremium",
+				},
+			},
+			quoteRequest: QuoteRequest{
+				DomainName:      "example.com",
+				Years:           1,
+				ClID:            "123456789",
+				TransactionType: TransactionTypeRegistration,
+				Currency:        "EUR",
+			},
+			expectedQuote: Quote{
+				DomainName: "example.com",
+				Years:      1,
+				Clid:       ClIDType("123456789"),
+				Price:      money.New(1200, "EUR"),
+				Phase:      &Phase{Name: "GA"},
+				Fees: []*Fee{
+					{
+						Name:     "Sunrise Fee",
+						Amount:   100,
+						Currency: "EUR",
+					},
+					{
+						Name:     "Verification Fee",
+						Amount:   1000,
+						Currency: "EUR",
+					},
+					{
+						Name:       "registration fee",
+						Amount:     100,
+						Currency:   "EUR",
+						Refundable: &tr,
+					},
+				},
+				FXRate: &FX{},
+				Class:  "lowPremium",
+			},
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			priceEngine := NewPriceEngine(tc.phase, tc.domain, tc.fx, tc.pl)
+			quote, err := priceEngine.GetQuote(tc.quoteRequest)
+			require.ErrorIs(t, err, tc.expectedError, "Error is not correct")
+			if tc.expectedError == nil {
+				require.Equal(t, tc.expectedQuote.DomainName, quote.DomainName, "DomainName is not correct")
+				require.Equal(t, tc.expectedQuote.Years, quote.Years, "Years are not correct")
+				require.Equal(t, tc.expectedQuote.Clid, quote.Clid, "Clid is not correct")
+				require.Equal(t, tc.expectedQuote.Price, quote.Price, "Price is not correct")
+				require.Equal(t, &tc.phase, quote.Phase, "Phase is not correct")
+				var expectedFees, actualFees []Fee
+				for _, fee := range tc.expectedQuote.Fees {
+					expectedFees = append(expectedFees, *fee)
+				}
+				for _, fee := range quote.Fees {
+					actualFees = append(actualFees, *fee)
+				}
+				require.Equal(t, expectedFees, actualFees, "Fees are not correct")
+				require.Equal(t, tc.expectedQuote.FXRate, quote.FXRate, "FxRate is not correct")
+				require.Equal(t, tc.expectedQuote.Class, quote.Class, "Class is not correct")
+			}
+		})
+	}
+
+}
