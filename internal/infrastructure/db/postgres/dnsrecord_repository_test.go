0e02b495391fa65af8caa27487c40278dd94bdfa
Geoff <geoffrey@deprins.net>
2024-08-02 22:57:19 -0500

    add fk with TLD

--- internal/infrastructure/db/postgres/dnsrecord_repository_test.go
+++ internal/infrastructure/db/postgres/dnsrecord_repository_test.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/onasunnymorning/domain-os/internal/domain/entities"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
 )
 
 type DNSRecordSuite struct {
 	suite.Suite
-	db *gorm.DB
+	db      *gorm.DB
+	tldName string
 }
 
 func TestDNSRecordSuite(t *testing.T) {
@@ -19,16 +21,29 @@ func TestDNSRecordSuite(t *testing.T) {
 
 func (s *DNSRecordSuite) SetupSuite() {
 	s.db = setupTestDB()
+
+	// Create a TLD
+	tld, err := entities.NewTLD("waves")
+	s.Require().NoError(err)
+	tldRepo := NewGormTLDRepo(s.db)
+	err = tldRepo.Create(context.Background(), tld)
+	s.Require().NoError(err)
+	s.tldName = tld.Name.String()
 }
 
 func (s *DNSRecordSuite) TearDownSuite() {
+	if s.tldName != "" {
+		tldRepo := NewGormTLDRepo(s.db)
+		err := tldRepo.DeleteByName(context.Background(), s.tldName)
+		s.Require().NoError(err)
+	}
 }
 
 func (s *DNSRecordSuite) TestCreateDNSRecord() {
 	// Create a DNS record
 	record := &DNSRecord{
-		Zone: "windy.waves",
-		Name: "www",
+		Zone: "waves",
+		Name: "windy",
 		Type: "A",
 		TTL:  300,
 		Data: `{"address": "192.168.0.1"}`,
@@ -52,22 +67,22 @@ func (s *DNSRecordSuite) TestGetDNSRecordsByZone() {
 	// Create some DNS records
 	records := []*DNSRecord{
 		{
-			Zone: "windy.waves",
-			Name: "www",
+			Zone: "waves",
+			Name: "windy",
 			Type: "A",
 			TTL:  300,
 			Data: `{"address": "192.168.0.1"}`,
 		},
 		{
-			Zone: "windy.waves",
-			Name: "mail",
+			Zone: "waves",
+			Name: "mail.windy",
 			Type: "MX",
 			TTL:  300,
 			Data: `{"preference": 10, "exchange": "mail.windy.waves"}`,
 		},
 		{
-			Zone: "windy.waves",
-			Name: "ns1",
+			Zone: "waves",
+			Name: "ns1.windy",
 			Type: "NS",
 			TTL:  300,
 			Data: `{"ns": "ns1.windy.waves"}`,
@@ -81,7 +96,18 @@ func (s *DNSRecordSuite) TestGetDNSRecordsByZone() {
 	}
 
 	// Get the DNS records
-	createdRecords, err := repo.GetByZone(context.Background(), "windy.waves")
+	createdRecords, err := repo.GetByZone(context.Background(), "waves")
 	s.Require().NoError(err)
 	s.Require().Len(createdRecords, 3)
+
+	// Delete the records
+	for _, record := range createdRecords {
+		err = repo.Delete(context.Background(), record.ID)
+		s.Require().NoError(err)
+	}
+
+	// Get the DNS records
+	createdRecords, err = repo.GetByZone(context.Background(), "windy.waves")
+	s.Require().NoError(err)
+	s.Require().Len(createdRecords, 0)
 }
