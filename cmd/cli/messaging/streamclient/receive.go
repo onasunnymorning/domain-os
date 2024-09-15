9ccd0e8e3f278710b8438db7e2389a5d06f768bd
Geoff <geoffrey@deprins.net>
2024-09-15 10:54:43 -0500

    Refactor receive.go to handle errors when converting port to int and creating stream environment

--- cmd/cli/messaging/streamclient/receive.go
+++ cmd/cli/messaging/streamclient/receive.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -11,17 +10,14 @@ import (
 	"github.com/rabbitmq/rabbitmq-stream-go-client/pkg/stream"
 )
 
-func CheckErrReceive(err error) {
-	if err != nil {
-		fmt.Printf("%s ", err)
-		os.Exit(1)
-	}
-}
 func main() {
 
 	portString := os.Getenv("RMQ_PORT")
 	portInt, err := strconv.Atoi(portString)
-	CheckErrReceive(errors.Join(errors.New("failed to convert port to int"), err))
+	if err != nil {
+		fmt.Printf("failed to convert port to int: %s", err)
+		os.Exit(1)
+	}
 
 	env, err := stream.NewEnvironment(
 		stream.NewEnvironmentOptions().
@@ -29,7 +25,10 @@ func main() {
 			SetPort(portInt).
 			SetUser(os.Getenv("RMQ_USER")).
 			SetPassword(os.Getenv("RMQ_PASS")))
-	CheckErrReceive(errors.Join(errors.New("failed to create stream environment"), err))
+	if err != nil {
+		fmt.Printf("failed to create stream environment: %s", err)
+		os.Exit(1)
+	}
 
 	streamName := os.Getenv("EVENT_STREAM_TOPIC")
 	err = env.DeclareStream(streamName,
@@ -37,7 +36,10 @@ func main() {
 			MaxLengthBytes: stream.ByteCapacity{}.GB(2),
 		},
 	)
-	CheckErrReceive(errors.Join(errors.New("failed to declare stream"), err))
+	if err != nil {
+		fmt.Printf("failed to declare stream: %s", err)
+		os.Exit(1)
+	}
 
 	messagesHandler := func(consumerContext stream.ConsumerContext, message *amqp.Message) {
 		fmt.Printf("Stream: %s - Received message: %s\n", consumerContext.Consumer.GetStreamName(),
@@ -46,16 +48,23 @@ func main() {
 
 	consumer, err := env.NewConsumer(streamName, messagesHandler,
 		stream.NewConsumerOptions().SetOffset(stream.OffsetSpecification{}.First()))
-	CheckErrReceive(errors.Join(errors.New("failed to create consumer"), err))
+	if err != nil {
+		fmt.Printf("failed to create consumer: %s", err)
+		os.Exit(1)
+	}
 
 	fmt.Println(" [x] Waiting for messages.")
 
 	// Optional: Add logic to gracefully close the consumer and environment on shutdown
 	defer func() {
 		err = consumer.Close()
-		CheckErrReceive(errors.Join(errors.New("failed to close consumer"), err))
+		if err != nil {
+			fmt.Printf("failed to close consumer: %s", err)
+		}
 		err = env.Close()
-		CheckErrReceive(errors.Join(errors.New("failed to close environment"), err))
+		if err != nil {
+			fmt.Printf("failed to close environment: %s", err)
+		}
 	}()
 
 	// Keep the program running indefinitely
