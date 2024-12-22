b05e0ce6533d4261c4a2c20b3530a1f0b35e384a
Geoff <geoffrey@deprins.net>
2024-12-22 10:30:15 -0500

    Fix container and volume removal in run.sh due to previous command failure

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export BRANCH=$(git branch --show-current)
 docker build -t geapex/domain-os:$BRANCH . && doppler run -- docker compose --profile essential -f docker-compose.yml up # --watch
 doppler run -- docker compose rm --force --volumes
+# the above stopped working for some reason, so I'm using the following instead
+docker container rm domain-os-db-1
+docker volume rm domain-os_db
