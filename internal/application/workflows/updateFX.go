ab59bf914d529e313fc9f08c6a10799402a58ca4
Geoff <geoffrey@deprins.net>
2025-01-01 09:50:17 -0500

    Add UpdateFX workflow for FX rate synchronization with retry policy

---
+++ internal/application/workflows/updateFX.go
@@ -0,0 +1,40 @@
+package workflows
+
+import (
+	"time"
+
+	"github.com/onasunnymorning/domain-os/internal/application/activities"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/workflow"
+)
+
+func UpdateFX(ctx workflow.Context) error {
+	// RetryPolicy specifies how to automatically handle retries if an Activity fails.
+	retrypolicy := &temporal.RetryPolicy{
+		InitialInterval:        time.Second,
+		BackoffCoefficient:     2.0,
+		MaximumInterval:        10 * time.Minute,
+		MaximumAttempts:        3, // 0 is unlimited retries
+		NonRetryableErrorTypes: []string{"none"},
+	}
+
+	options := workflow.ActivityOptions{
+		// Timeout options specify when to automatically timeout Activity functions.
+		StartToCloseTimeout: time.Minute,
+		// Optionally provide a customized RetryPolicy.
+		// Temporal retries failed Activities by default.
+		RetryPolicy: retrypolicy,
+	}
+
+	// Apply the options.
+	ctx = workflow.WithActivityOptions(ctx, options)
+
+	// Update USD
+	updateErr := workflow.ExecuteActivity(ctx, activities.UpdateFX("usd")).Get(ctx, nil)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	return nil
+
+}
