3047cb6b59ee0be598d481ee8b7585a7c1eead16
Geoff <geoffrey@deprins.net>
2025-01-03 09:45:45 -0500

    Update ContactDataPolicy to use 'mandatory' instead of 'required' for registrant and tech contact data

--- internal/domain/entities/contactDataPolicy.go
+++ internal/domain/entities/contactDataPolicy.go
@@ -1,8 +1,8 @@
 package entities
 
 type ContactDataPolicy struct {
-	RegistrantContactDataPolicy ContactDataPolicyType `json:"registrantDataContactPolicy,omitempty" example:"required"`
-	TechContactDataPolicy       ContactDataPolicyType `json:"techContactDataPolicy,omitempty" example:"required"`
+	RegistrantContactDataPolicy ContactDataPolicyType `json:"registrantDataContactPolicy,omitempty" example:"mandatory"`
+	TechContactDataPolicy       ContactDataPolicyType `json:"techContactDataPolicy,omitempty" example:"mandatory"`
 	AdminContactDataPolicy      ContactDataPolicyType `json:"adminContactDataPolicy,omitempty" example:"optional"`
 	BillingContactDataPolicy    ContactDataPolicyType `json:"billingContactDataPolicy,omitempty" example:"prohibited"`
 }
@@ -12,7 +12,7 @@ type ContactDataPolicyType string
 
 // ContactPolicy value object consists of all the settings of a TLD that can be changed in a phase roll
 const (
-	ContactDataPolicyTypeRequired   = ContactDataPolicyType("required")
+	ContactDataPolicyTypeMandatory  = ContactDataPolicyType("mandatory")
 	ContactDataPolicyTypeOptional   = ContactDataPolicyType("optional")
 	ContactDataPolicyTypeProhibited = ContactDataPolicyType("prohibited")
 )
@@ -20,8 +20,8 @@ const (
 // ContactPolicy factory. This returns a new ContactPolicy object with default values (Registrant and Tech are required, Admin and Billing are optional)
 func NewContactPolicy() ContactDataPolicy {
 	return ContactDataPolicy{
-		RegistrantContactDataPolicy: ContactDataPolicyTypeRequired,
-		TechContactDataPolicy:       ContactDataPolicyTypeRequired,
+		RegistrantContactDataPolicy: ContactDataPolicyTypeMandatory,
+		TechContactDataPolicy:       ContactDataPolicyTypeMandatory,
 		AdminContactDataPolicy:      ContactDataPolicyTypeOptional,
 		BillingContactDataPolicy:    ContactDataPolicyTypeOptional,
 	}
--- internal/domain/entities/contactDataPolicy_test.go
+++ internal/domain/entities/contactDataPolicy_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestNewContactPolicy(t *testing.T) {
 	cp := NewContactPolicy()
 
-	if cp.RegistrantContactDataPolicy != ContactDataPolicyTypeRequired {
-		t.Errorf("expected RegistrantPolicy to be %q, got %q", ContactDataPolicyTypeRequired, cp.RegistrantContactDataPolicy)
+	if cp.RegistrantContactDataPolicy != ContactDataPolicyTypeMandatory {
+		t.Errorf("expected RegistrantPolicy to be %q, got %q", ContactDataPolicyTypeMandatory, cp.RegistrantContactDataPolicy)
 	}
-	if cp.TechContactDataPolicy != ContactDataPolicyTypeRequired {
-		t.Errorf("expected TechPolicy to be %q, got %q", ContactDataPolicyTypeRequired, cp.TechContactDataPolicy)
+	if cp.TechContactDataPolicy != ContactDataPolicyTypeMandatory {
+		t.Errorf("expected TechPolicy to be %q, got %q", ContactDataPolicyTypeMandatory, cp.TechContactDataPolicy)
 	}
 	if cp.AdminContactDataPolicy != ContactDataPolicyTypeOptional {
 		t.Errorf("expected AdminPolicy to be %q, got %q", ContactDataPolicyTypeOptional, cp.AdminContactDataPolicy)
