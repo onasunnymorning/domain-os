a42732e9f367195838bc0ba95b6a3121715a080d
Geoff <geoffrey@deprins.net>
2025-01-08 15:33:53 -0500

    refactor: Enhance DomainLifeCycleEvent struct with additional CorrelationID field and improved comments for clarity

--- internal/domain/entities/domainLifecycleEvent.go
+++ internal/domain/entities/domainLifecycleEvent.go
@@ -16,16 +16,17 @@ var (
 
 // DomainLifeCycleEvent struct defines a billing event that is generated each time a domain is registered, renewed, transferred or deleted
 type DomainLifeCycleEvent struct {
-	ClientID        string
-	ResellerID      string
-	TldName         string
-	DomainName      string
-	DomainYears     int
-	TransactionType TransactionType
-	SKU             string
-	Quote           Quote
-	TraceID         string
-	TimeStamp       time.Time
+	ClientID        string          // ClientID is the unique identifier of the client Registrar.ClID
+	ResellerID      string          // ResellerID is the unique identifier of the reseller if applicable
+	TldName         string          // TldName is the top level domain name (e.g. COM, NET, ORG)
+	DomainName      string          // DomainName is the domain name (e.g. example.net)
+	DomainYears     int             // DomainYears is the number of years the transaction is for
+	TransactionType TransactionType // TransactionType is the type of transaction (e.g. REGISTRATION, RENEWAL, TRANSFER, DELETE)
+	SKU             string          // SKU is the Stock Keeping Unit of the transaction (e.g. COM-REGISTRATION-1)
+	Quote           Quote           // Quote is the quote of the transaction
+	TraceID         string          // TraceID is the unique identifier of the transaction
+	CorrelationID   string          // CorrelationID is a link to an upstream event if applicable
+	TimeStamp       time.Time       // TimeStamp is the time the transaction took place
 }
 
 // NewDomainLifeCycleEvent creates a new DomainLifeCycleEvent with the given parameters
