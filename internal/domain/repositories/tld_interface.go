a7a0084f5657c69a1c9baf852f5bf58b462547e3
Geoff <geoffrey@deprins.net>
2025-01-27 06:57:36 -0500

    feat: add AllowEscrowImport and EnableDNS fields to TLD, implement SetAllowEscrowImport method, and enhance error handling in TLDService

--- internal/domain/repositories/tld_interface.go
+++ internal/domain/repositories/tld_interface.go
@@ -10,7 +10,7 @@ type TLDRepository interface {
 	Create(ctx context.Context, tld *entities.TLD) error
 	GetByName(ctx context.Context, name string, preloadAll bool) (*entities.TLD, error)
 	List(ctx context.Context, pageSize int, pageCursor string) ([]*entities.TLD, error)
-	// Update(tld *entities.TLD) error
+	Update(ctx context.Context, tld *entities.TLD) error
 	DeleteByName(ctx context.Context, name string) error
 	Count(ctx context.Context) (int64, error)
 }
