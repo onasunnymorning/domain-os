21d4a921cdde22e0263ccf91b63590cafdb71f74
Geoff <geoffrey@deprins.net>
2025-03-17 15:22:47 -0500

    feat: add ListContactsFilter for query parameter construction and implement tests

--- internal/application/queries/listContactsFilter.go
+++ internal/application/queries/listContactsFilter.go
@@ -1,9 +1,37 @@
 package queries
 
+// ListContactsFilter is a filter for the ListContacts query
 type ListContactsFilter struct {
+	RoidGreaterThan string
+	RoidLessThan    string
+	IdLike          string
+	EmailLike       string
+	ClidEquals      string
 }
 
+// ToQueryParams converts the filter to query parameters
 func (f ListContactsFilter) ToQueryParams() string {
-	queryString := ""
-	return queryString
+	var queryParams string
+
+	if f.RoidGreaterThan != "" {
+		queryParams += "&roid_greater_than=" + f.RoidGreaterThan
+	}
+
+	if f.RoidLessThan != "" {
+		queryParams += "&roid_less_than=" + f.RoidLessThan
+	}
+
+	if f.IdLike != "" {
+		queryParams += "&id_like=" + f.IdLike
+	}
+
+	if f.EmailLike != "" {
+		queryParams += "&email_like=" + f.EmailLike
+	}
+
+	if f.ClidEquals != "" {
+		queryParams += "&clid_equals=" + f.ClidEquals
+	}
+
+	return queryParams
 }
